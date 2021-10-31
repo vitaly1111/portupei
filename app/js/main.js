34945f97a7578ff05cd4c52e59b1c0f199db26cc
me me <timb0702@gmail.com>
2021-10-31 20:39:02 +0700

    slider html added

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,18 @@
 
+import Swiper from 'swiper';
+
+import '../node_modules/swiper/dist/css/swiper.css';
+// import Swiper styles
+
+
+
+const swiper=new Swiper('.swiper',{
+	// Optional parameters
+
+	loop: true,
+
+
+});
+
+
 //# sourceMappingURL=main.js.map
